1c0d118cdf9c679b17fd1e392b3a5db2888533b0
acpuchades <59510094+acpuchades@users.noreply.github.com>
2025-05-02 19:19:05 +0200

    Use quarto render engine

--- flake.nix
+++ flake.nix
@@ -18,8 +18,7 @@
 				gtsummary
 				janitor
 				missForest
-				powerSurvEpi
-				rmarkdown
+				quarto
 				survival
 				tidyverse
 			];
@@ -27,21 +26,21 @@
 			rstudio-with-packages = pkgs.rstudioWrapper.override { packages = r-packages; };
 			render-cmd = pkgs.writeShellApplication {
 				name = "render";
-				runtimeInputs = [ r-with-packages ];
+				runtimeInputs = [ pkgs.pandoc pkgs.texliveFull r-with-packages ];
 				text = ''
-					${r-with-packages}/bin/Rscript -e "rmarkdown::render('tcd-als-immunology.Rmd')"
+					${r-with-packages}/bin/Rscript -e "quarto::quarto_render('tcd-als-immunology.qmd')"
 				'';
 			};
 			rstudio-cmd = pkgs.writeShellApplication {
 				name = "rstudio";
 				runtimeInputs = [ rstudio-with-packages ];
 				text = ''
-					${rstudio-with-packages}/bin/rstudio tcd-als-immunology.Rmd
+					${rstudio-with-packages}/bin/rstudio ./tcd-als-immunology.qmd
 				'';
 			};
 		in {
 			devShells.default = pkgs.mkShell {
-				packages = [ pkgs.ghostscript r-with-packages rstudio-with-packages ];
+				packages = [ r-with-packages rstudio-with-packages ];
 			};
 			apps.rstudio = {
 				type = "app";
